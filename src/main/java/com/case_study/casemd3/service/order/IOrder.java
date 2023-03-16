d8bd718a1247c588f189673539bf7ca06f6cf1cf
unknown <nguyentuan24072015@gmail.com>
2023-03-16 14:00:18 +0700

    upload to template trua nay an food j

--- src/main/java/com/case_study/casemd3/service/order/IOrder.java
+++ src/main/java/com/case_study/casemd3/service/order/IOrder.java
@@ -1,6 +1,7 @@
 package com.case_study.casemd3.service.order;
 
+import com.case_study.casemd3.model.Order;
 import com.case_study.casemd3.service.IGeneric;
 
-public interface IOrder extends IGeneric {
+public interface IOrder extends IGeneric<Order> {
 }
