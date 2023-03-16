dc13eb479a3999d453d62ddbb6b5dec5b7c0db31
luc-nguyen-174 <louiskingvn@gmail.com>
2023-03-16 16:28:56 +0700

    update oder

--- src/main/java/com/case_study/casemd3/service/cart/CartService.java
+++ src/main/java/com/case_study/casemd3/service/cart/CartService.java
@@ -245,4 +245,6 @@ public class CartService implements ICart {
         }
         return rowDisable;
     }
+
+
 }
---
+++ src/main/java/com/case_study/casemd3/service/order/OderService.java
@@ -0,0 +1,31 @@
+package com.case_study.casemd3.service.order;
+
+import java.util.List;
+
+public class OderService implements IOrder{
+    OderService oderService = new OderService();
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Object generic) {
+
+    }
+
+    @Override
+    public Object findById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean update(int id, Object generic) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        return false;
+    }
+}
